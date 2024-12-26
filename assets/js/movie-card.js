d2556b72ff55a7508b2914bc46e3932c88f7007c
xsmendoza <em06032007@gmail.com>
2024-12-26 09:05:13 -0600

    Update movie-card.js

--- assets/js/movie-card.js
+++ assets/js/movie-card.js
@@ -41,7 +41,7 @@ export function createMovieCard(movie) {
       <div class="card-badge">${release_date.split("-")[0]}</div>
     </div>
 
-    <a href="./detail.html" class="card-btn" title="${title}"></a>
+    <a href="./detail.html?=${formattedTitle}"  class="card-btn" title="${title}"></a>
   `;
 
   return card;
